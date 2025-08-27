1ed5448cc24319655820eb9b44c5536bb4f86609
harshrey <smhardas11@gmail.com>
2025-08-27 14:34:00 -0700

    fix: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/harshrey.github.io/',
   plugins: [react()],
   build: {
     outDir: './dist',
